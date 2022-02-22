d800cfa9befa7bd17ee3ef68746b9e4eb0abd18c
Yuriy Oliferchuk <dbzero86@gmail.com>
2022-02-22 12:10:28 +0200

    test5

--- server.js
+++ server.js
@@ -18,31 +18,31 @@ app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//session config
-// app.use(flash())
-// app.use(session({
-//     name: process.env.SESS_NAME,
-//     resave: false,
-//     saveUninitialized: false,
-//     secret: process.env.SESS_SECRET,
-//     cookie: {
-//       maxAge: 1000*60*60*2,
-//       sameSite: true,
-//       secure: false,
-//     }
-// }))
+// session config
+app.use(flash())
+app.use(session({
+    name: sid,
+    resave: false,
+    saveUninitialized: false,
+    secret: "XXXX",
+    cookie: {
+      maxAge: 1000*60*60*2,
+      sameSite: true,
+      secure: false,
+    }
+}))
   
 // Passport Config
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(passport.authenticate('session'));
 
-// app.use('/', interface)
+app.use('/', interface)
 app.use('/api', api)
 
-app.get('/', (req, res)=>{
-    res.render('index', {isLogin: false})
-})
+// app.get('/', (req, res)=>{
+//     res.render('index', {isLogin: false})
+// })
 
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}/`)
